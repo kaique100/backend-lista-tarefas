2d2d8fec78804a70fd95fbf926abfeb0878e1067
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-03 22:19:59 -0300

    feito os verbos put e delete

--- server.js
+++ server.js
@@ -33,14 +33,29 @@ app.get('/tarefas', (req, res) => {
 
 // Rota para editar uma tarefa existente
 app.put('/tarefas/:id', (req, res) => {
-    // Lógica para editar uma tarefa no banco de dados com base no ID
+  // Lógica para obter e retornar todas as tarefas do banco de dados
+  const {id} = req.params;
+  const {tarefa} = req.body;
+  const index = tarefas.findIndex(t => t.id === parseInt(id));
+
+  if (index != -1) {
+    tarefas[index].tarefa = tarefa;
+    res.status(200).json({ message: 'Tarefa atualizada com sucesso!'})
+  }else{
+    res.status(404).json({error: 'Tarefa não encontrada!'});
+  }
 });
 
 // Rota para excluir uma tarefa
-app.delete('/tarefas/:id', (req, res) => {
-    // Lógica para excluir uma tarefa do banco de dados com base no ID
-});
+app.delete('/tarefas/:id', (req, res) => { 
+   const { id } = req.params;  
+   const index = tarefas.findIndex(t => t.id === parseInt(id));  
+   if (index !== -1) {    
+    tarefas.splice(index, 1);    
+    res.status(200).json({ message: 'Tarefa removida com sucesso!' });  
+  } else {   
+     res.status(404).json({ error: 'Tarefa não encontrada!' });  }});
 // Inicie o servidor Express:
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta em http;//localhost:${PORT}`);
+    console.log(`Servidor rodando na porta em http://localhost:${PORT}`);
 });
