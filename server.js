a3228aaf3c2f903afde6c12c8abd4d1b5b68737d
Kaique <skaique5424@gmail.com>
2024-05-22 22:58:04 -0300

    tarefa de inclusão de cadastrado e login usando JWT e testado no postman

--- server.js
+++ server.js
@@ -1,22 +1,44 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose()
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const PORT = 3000; // Defina a porta do servidor
+app.use(cors());    
 
 const db = new sqlite3.Database('banco-de-dados.db')
-app.use(cors());
 
 // Criar a tabela 'tarefas' no banco de dados
 db.serialize(() => {
     db.run("CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, task TEXT)");
 });
 
+// Criar a tabela 'usuarios' no banco de dados
+db.serialize(() => {
+    db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT UNIQUE, password TEXT, role TEXT)");
+});
+
+const verifyToken = (req, res, next) => {
+    const token = req.headers['authorization'];
+    if (!token) {
+        return res.status(403).json({ error: 'Nenhum token fornecido.' });
+    }
+    jwt.verify(token.split(' ')[1], 'secreto', (err, decoded) => {
+        if (err) {
+            return res.status(500).json({ error: 'Falha ao autenticar o token.' });
+        }
+        req.userId = decoded.id;
+        req.userRole = decoded.role;
+        next();
+    });
+};
+
 app.use(express.json());
 
 // Rota para adicionar uma nova tarefa
-app.post('/tarefas', (req, res) => {
+app.post('/tarefas', verifyToken, (req, res) => {
     const { task } = req.body;
     // Inserir a nova tarefa no banco de dados
     db.run("INSERT INTO tasks (task) VALUES (?)", [task], function(err) {
@@ -28,7 +50,7 @@ app.post('/tarefas', (req, res) => {
 });
 
 // Rota para obter todas as tarefas
-app.get('/tarefas', (req, res) => {
+app.get('/tarefas', verifyToken,  (req, res) => {
     // Obter todas as tarefas do banco de dados
     db.all("SELECT * FROM tasks", [], (err, rows) => {
         if (err) {
@@ -39,7 +61,7 @@ app.get('/tarefas', (req, res) => {
 });
 
 // Rota para obter uma tarefa específica
-app.get('/tarefas/:id', (req, res) => {
+app.get('/tarefas/:id', verifyToken, (req, res) => {
     const { id } = req.params;
     // Obter a tarefa pelo ID
     db.get("SELECT * FROM tasks WHERE id = ?", [id], (err, row) => {
@@ -55,7 +77,7 @@ app.get('/tarefas/:id', (req, res) => {
 });
 
 // Rota para editar uma tarefa existente
-app.put('/tarefas/:id', (req, res) => {
+app.put('/tarefas/:id', verifyToken, (req, res) => {
     const { id } = req.params;
     const { task } = req.body;
     // Atualizar a tarefa com base no ID
@@ -72,7 +94,7 @@ app.put('/tarefas/:id', (req, res) => {
 });
 
 // Rota para excluir uma tarefa
-app.delete('/tarefas/:id', (req, res) => {
+app.delete('/tarefas/:id', verifyToken, (req, res) => {
     const { id } = req.params;
     // Excluir a tarefa com base no ID
     db.run("DELETE FROM tasks WHERE id = ?", [id], function(err) {
@@ -86,6 +108,67 @@ app.delete('/tarefas/:id', (req, res) => {
         }
     });
 });
+
+// Rota para registrar um novo usuário
+app.post('/registro', async (req, res) => {
+    const { username, password, role } = req.body;
+    try {
+        const existinigUser = await findUser(username);
+        if (existinigUser) {
+            return res.status(400).json({ error: 'Usuário já registrado' });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await createUser(username, hashedPassword, role);
+        res.status(201).json({ message: 'Usuário registrado com sucesso' });
+    } catch (error) {
+        console.error('Erro no registro:', error);
+        res.status(500).json({ error: 'Erro no registro de usuário' });
+    }
+});
+
+// Rota para autenticar o usuário e gerar token JWT
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = await findUser(username);
+        if (!user) {
+            return res.status(401).json({ error: 'Usuário não encontrado' });
+        }
+        const ValidPasswork = await bcrypt.compare(password, user.password);
+        if (!ValidPasswork) {
+            return res.status(401).json({ error: 'Senha incorreta' });
+        }
+        const token = jwt.sign({ id: user.id, username: user.username, role: user.role }, 'secreto', { expiresIn: '1h' });
+        res.status(200).json({ token });
+    } catch (error) {
+        console.error('Erro no login:', error);
+        res.status(500).json({ error: 'Erro no login de usuário' });
+    }
+});
+
+// Função para buscar usuário no banco de dados
+const findUser = (username) => {
+    return new Promise((resolve, reject) => {
+        db.get('SELECT * FROM users WHERE username = ?', [username], (err, row) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(row);
+        });
+    });
+};
+const createUser = (username, password, role) => {
+    return new Promise((resolve, reject) => {
+        db.run('INSERT INTO users (username, password, role) VALUES (?, ?, ?)', [username, password, role], (err) => {
+            if (err) {
+                reject(err);
+            }
+            resolve();
+        });
+    });
+};
+
+
 
 // Inicie o servidor Express
 app.listen(PORT, () => {
